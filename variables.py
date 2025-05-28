d247d038cabf2c3285837f60f395e38ba6f681e8
chandu799525 <chandu799525@gmail.com>
2025-05-28 17:33:01 +0530

    variables

---
+++ variables.py
@@ -0,0 +1,35 @@
+x=56            #int
+y=6.56          #float
+complex_=2+3j   #complex
+z=True          #bool
+a="python"      #string
+b=[1,3,4,5,6]    #list
+c=(23,51,43,34)   #tuple
+d={5,14,21,53,45}  #set
+dictionary={'chandu':'teeparru','rayudu':'ravulapalem','suchendra':'palangi'}  #dictionary
+
+#range(stop)
+#range(start, stop)
+#range(start, stop, step)
+range(5)
+print(list(range(5)))
+range(2,6)
+print(list(range(2,6)))
+range(1,6,2)
+print(set(range(1,6,2)))
+
+range(6,2)
+print(list(range(6,0,-1)))
+ 
+lst=reversed(range(6))     
+print(list(reversed(range(6))))
+
+range(-6,-1)
+print(list(range(-6,-1)))
+range(-6,-1,2)
+print(list(range(-6,-1,2)))
+range(-1,-6,)
+print(list(range(-1,-6,-2)))
+
+
+
