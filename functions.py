d472c99c03b5b792d11752d94a07b920ca8d5579
chandu799525 <chandu799525@gmail.com>
2025-06-04 18:00:41 +0530

    functions

---
+++ functions.py
@@ -0,0 +1,14 @@
+def fun():  # function creation
+    print("new function")
+
+fun() # function calling
+
+def p(name:str) ->  int: # function with parameter
+    print(f"hello, {name}")
+    
+def p(n) :
+    print(n)
+p("chandu")   # passing arguments
+
+
+
